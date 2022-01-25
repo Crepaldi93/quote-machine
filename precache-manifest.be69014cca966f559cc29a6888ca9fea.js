f98d696b134c0c126dcd8a3fdf0d52582747585b
Crepaldi93 <96325460+Crepaldi93@users.noreply.github.com>
2022-01-25 12:59:44 -0800

    Updates

---
+++ precache-manifest.be69014cca966f559cc29a6888ca9fea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74c4985eabaae0ad52ef3787a57b432",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "a63e7a9008da380630da",
+    "url": "/quote-machine/static/css/main.791aa26c.chunk.css"
+  },
+  {
+    "revision": "231a0ac7371d52dcbf8b",
+    "url": "/quote-machine/static/js/2.b5579283.chunk.js"
+  },
+  {
+    "revision": "337690bdb71522eaada8e2b2e5b3e79a",
+    "url": "/quote-machine/static/js/2.b5579283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa1f5bcc67d549b9a69",
+    "url": "/quote-machine/static/js/3.1c020417.chunk.js"
+  },
+  {
+    "revision": "a63e7a9008da380630da",
+    "url": "/quote-machine/static/js/main.21db4c2e.chunk.js"
+  },
+  {
+    "revision": "894415f3f1c59f32e63c",
+    "url": "/quote-machine/static/js/runtime-main.d795ed3d.js"
+  }
+]);
